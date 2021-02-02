72812fbc8a8e256d89e9469e579045e5bf3d39da
Vitali <vitaliken@gmail.com>
2021-02-02 20:00:50 +0300

    dz2ok

---
+++ js/script1.js
@@ -0,0 +1,83 @@
+// if (1){
+//     console.log('Ok!')
+// } else {
+//     console.log('Error');
+// }
+
+// const num = 50;
+
+// if (num < 49){
+//     console.log('Errorrr');
+// } else if (num > 100){
+//     console.log('Many');
+// } else {
+//     console.log('OK!!!!!');
+// }
+
+// (num === 50) ? console.log('OK!!!!!') : console.log('Errorrr');
+
+// const num = 49;
+
+// switch (num) {
+//     case 49:
+//         console.log('Неверно');
+//         break;
+//     case 100:
+//         console.log('Неверно');
+//         break;
+//     case 50:
+//         console.log('Верняк');
+//         break;
+//     default:
+//         console.log('Не в этот раз');
+//         break;
+// }
+
+
+
+// let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// let num = 50;
+// do {
+//     console.log(num);
+//     num++; 
+// }
+// while(num < 55);
+
+
+// let num = 50;
+
+// for(let i = 1; i < 10; i++) {
+//     if(i === 7){
+//         // break;
+//         continue;
+//     }
+
+//     console.log(i);
+// }
+
+
+
+
+// for (let i = 0; i < 3; i++) {
+//     alert( `number ${i}!` );
+//   }
+
+// let i = 0;
+// while (i < 3) {
+//     console.log(`number ${i}!`);
+//     i++;
+// }
+
+
+
+// let num;
+// do {
+//     num = prompt('Введите число, большее 100', 0);
+// } while (num <= 100 && num);
